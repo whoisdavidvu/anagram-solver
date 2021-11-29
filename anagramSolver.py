53ea2a33090d47c483d1001809518b868f396565
David Vu <davidvu396@googlemail.com>
2021-11-29 15:19:00 +0100

    added comments and createPermutations function

--- anagramSolver.py
+++ anagramSolver.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
 import random
+import itertools
 
+# Scrambles the letters of the given word.
 def scrambleWord(toScramble):
     scrambled = list(toScramble)
     random.shuffle(scrambled)
     return ''.join(scrambled)
 
-
+# Creates all permutations of the given word and stores it in an array.
+def createPermutations(word):
+    permutations = ["".join(perm) for perm in itertools.permutations(word)]
+    return permutations
 
 while True:
-    anagram = raw_input("Enter the anagram to be solved: ")
-    print (scrambleWord(anagram))
+    anagram = input("Enter the anagram to be solved: ")
+    """ print ("Scrambled word:", scrambleWord(anagram)) """
+
+    print (createPermutations(anagram))
     break
